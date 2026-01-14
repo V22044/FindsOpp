f4a1cc75a1df5e2ff689d3d4e2ba0339a1e5d3d4
Vick <153551836+V22044@users.noreply.github.com>
2026-01-14 18:07:15 +0000

    add home screen
    still need fixing

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/components/screens/Home";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const NavStack = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        title: "Home",
+      },
+    },
   },
-});
+  {
+    initialRouteName: "Home",
+  }
+);
+const App = () => {
+  return (
+    <NavigationContainer>
+      <NavStack.Navigator>
+        <NavStack.Screen name="Home" component={Home} />
+      </NavStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/components/screens/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Screen!</Text>
+    </View>
+  );
+};
+
+export default Home;
